8a07e25de6b393252ea95a6ef97c4dbf2382def2
00swinter <rasmus.klaukien@gmail.com>
2026-01-04 01:25:09 +0100

    fix base path

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [svelte()],
+  base: '/svelte_portfolio_webpage/',
   build: {
     outDir: 'docs', // Changes output from 'dist' to 'docs'
   }
